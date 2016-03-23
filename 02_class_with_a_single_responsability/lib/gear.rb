dd3a8537c64954aed31f55b9250edeaf7da975d8
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2016-03-22 21:59:36 -0600

    New behavior, create instance with four attributes

--- 02_class_with_a_single_responsability/lib/gear.rb
+++ 02_class_with_a_single_responsability/lib/gear.rb
@@ -1,10 +1,12 @@
 class Gear
 
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog       = cog
+    @rim       = rim
+    @tire      = tire 
   end
 
   def ratio
--- 02_class_with_a_single_responsability/spec/gear_spec.rb
+++ 02_class_with_a_single_responsability/spec/gear_spec.rb
@@ -3,21 +3,23 @@ require 'gear'
 RSpec.describe Gear do
   
   describe "#Initialize" do
-    it 'create new instance with two attributes' do
-      @gear = Gear.new(52,11)
+    it 'Create instance with chainring, cog, rim, tire' do
+      @gear = Gear.new(52,11,26,1.5)
       expect(@gear.chainring).to eq 52 
       expect(@gear.cog).to eq 11 
+      expect(@gear.rim).to eq 26 
+      expect(@gear.tire).to eq 1.5 
     end
   end
 
   describe '#ratio' do
     it 'when chainring is 52 and cog is 11 the ratio is 4.7' do
-      @gear = Gear.new(52,11)
+      @gear = Gear.new(52,11,0,0)
       expect(@gear.ratio.round(1)).to eq 4.7
     end
 
     it 'when chainring is 30 and cog is 27 the ratio is 1.1' do
-      @gear = Gear.new(30,27)
+      @gear = Gear.new(30,27,0,0)
       expect(@gear.ratio.round(1)).to eq 1.1
     end
   end
