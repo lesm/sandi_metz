f749058e442321a2602087120939c80815000aac
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2016-03-23 03:11:09 -0600

    Create method #diameters

--- 02_class_with_a_single_responsability/lib/obscuring_references.rb
+++ 02_class_with_a_single_responsability/lib/obscuring_references.rb
@@ -3,4 +3,9 @@ class ObscuringReferences
   def initialize(data)
     @data = data  
   end
+
+  def diameters
+    data.collect { |cell| cell[0] + (cell[1] * 2)}
+  end
+
 end
--- 02_class_with_a_single_responsability/spec/obscuringReferences_spec.rb
+++ 02_class_with_a_single_responsability/spec/obscuringReferences_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe ObscuringReferences do
       expect(@obscurin_references.data).to include([622,20],[559,30])  
     end
   end
+  describe '#diameters' do
+    it 'when data is [[622,20],[559,30]], should be [662,619]' do
+      data = [[622,20],[559,30]]
+      @obscuring_references = ObscuringReferences.new(data)
+      expect(@obscuring_references.diameters).to eq ([662,619])
+    end
+    it 'when data is [[622,20],[622,23],[559,30],[559,40]] should be [662,668,619,639]' do
+      data = [[622,20],[622,23],[559,30],[559,40]]
+      @obscuring_references = ObscuringReferences.new(data)
+      expect(@obscuring_references.diameters).to eq ([662,668,619,639])
+    end
+  end
 end
