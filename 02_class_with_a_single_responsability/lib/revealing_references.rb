045fa01b4fe143e6cfb105ad93103ff0382c8219
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2016-03-24 16:50:55 -0600

    Refactoring of method initialize, of RevealingReferences class

--- 02_class_with_a_single_responsability/lib/revealing_references.rb
+++ 02_class_with_a_single_responsability/lib/revealing_references.rb
@@ -1,6 +1,11 @@
 class RevealingReferences
   attr_reader :wheels
   def initialize(data)
-    @wheels = data
+    @wheels = wheelify(data)
   end  
+
+  Wheel = Struct.new(:rim,:tire)
+  def wheelify(data)
+    data.collect { |cell| Wheel.new(cell[0],cell[1]) }
+  end
 end
--- 02_class_with_a_single_responsability/spec/revealing_refereces_spec.rb
+++ 02_class_with_a_single_responsability/spec/revealing_refereces_spec.rb
@@ -3,9 +3,12 @@ require 'revealing_references'
 RSpec.describe RevealingReferences do 
   describe '#Initialize' do
     it 'Create instance with two-dimensional array' do
-      wheels = [[500,20],[400,30],[600,40]]
+      Wheel = Struct.new(:rim,:tire)
+      wheel = Wheel.new(500,20)
+      wheels = [[500,20]]
       @revealing_references = RevealingReferences.new(wheels)
-      expect(@revealing_references.wheels).to include([500,20],[400,30],[600,40]) 
+      expect(@revealing_references.wheels.first.rim).to eq(wheel.rim) 
+      expect(@revealing_references.wheels.first.tire).to eq(wheel.tire) 
     end
   end
 end
